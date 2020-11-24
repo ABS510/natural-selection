de220d03f95e26631940eb87dcb3dde887876fff
Akshay Sivaraman <abs7@illinois.edu>
2020-11-24 20:33:54 +0530

    Created class to display the environment

---
+++ include/visualizer/environment_display.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cinder/gl/gl.h>
+#include "core/environment.h"
+
+namespace naturalselection {
+
+/**
+ * Class for displaying the environment and everything contained within it
+ */
+class EnvironmentDisplay {
+ public:
+  /**
+   * Constructor to initialize the EnvironmentDisplay object
+   * @param environment The environment object to be displayed
+   * @param top_left_corner The top left corner of the environment
+   */
+  EnvironmentDisplay(Environment environment, glm::vec2 top_left_corner);
+
+  void Draw();
+
+ private:
+  Environment environment_;
+  glm::vec2 top_left_corner_;
+
+  /**
+   * Function to draw a Food object
+   * @param food The food object to be drawn
+   */
+  void DrawFood(const Food& food);
+
+  /**
+   * Function to draw an Organism
+   * @param organism The organism to be drawn
+   */
+  void DrawOrganism(const Organism& organism);
+
+  /**
+   * Function to draw the environment
+   */
+  void DrawEnvironment();
+};
+}
