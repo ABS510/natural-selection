c30547612a4dcd90257bc791fdf56251e6cd754d
Akshay Sivaraman <abs7@illinois.edu>
2020-11-24 20:03:44 +0530

    Added a constructor and AddFood()

--- include/core/environment.h
+++ include/core/environment.h
@@ -12,16 +12,58 @@ namespace naturalselection {
 class Environment {
  public:
   /**
-   * Default constructor
-   */
-  Environment();
-
-  /**
-   * Constructor to initialize envoronment with length and height
+   * Constructor to initialize environment with length and height
    * @param length Length of the (rectangular) environment
    * @param height height of the environment
    */
-  Environment(double length, double height);
+  Environment(int length, int height);
+
+  /**
+   * Constructor to initialize environment with length and height, as well
+   * as the total number of calories of the environment and the number of
+   * organisms to start with.
+   * @param length Length of the (rectangular) environment
+   * @param height height of the environment
+   * @param num_starting_organisms The number of organisms at the start of the
+   * simulation
+   * @param total_calories The number of calories (in food) in the environment
+   * @param num_food The number of food objects in the environment
+   */
+  Environment(int length, int height, double total_calories, int num_food,
+              int num_starting_organisms);
+
+  /**
+   * Constructor to initialize environment with length and height, as well
+   * as the total number of calories of the environment and the number of
+   * organisms to start with.
+   * @param length Length of the (rectangular) environment
+   * @param height height of the environment
+   * @param num_starting_organisms The number of organisms at the start of the
+   * simulation
+   * @param total_calories The number of calories (in food) in the environment
+   * @param num_food The number of food objects in the environment
+   * @param length_of_day The number of frames for which each day will last for
+   */
+  Environment(int length, int height, double total_calories, int num_food,
+              int num_starting_organisms, int length_of_day);
+
+  /**
+  * Constructor to initialize environment with length and height, as well
+  * as the total number of calories of the environment and the number of
+  * organisms to start with.
+  * @param length Length of the (rectangular) environment
+  * @param height height of the environment
+  * @param num_starting_organisms The number of organisms at the start of the
+      * simulation
+  * @param total_calories The number of calories (in food) in the environment
+  * @param num_food The number of food objects in the environment
+  * @param length_of_day The number of frames for which each day will last for
+  * @param starting_speed The speed of the first generation of organisms
+  * @param starting_size The size of the first generation of organisms
+  */
+  Environment(int length, int height, double total_calories, int num_food,
+              int num_starting_organisms, int length_of_day,
+              double starting_speed, double starting_size);
 
   /**
    * Function to reset the environment after the end of each day
@@ -32,19 +74,42 @@ class Environment {
    * Function to update the state of the environment. Called every frame.
    */
   void Update();
+
+  /**
+   * Getter method for length
+   * @return length_
+   */
+  int length() {
+    return length_;
+  }
+
+  /**
+   * Getter method for height
+   * @return height_
+   */
+  int height() {
+    return height_;
+  }
+
  private:
-  const double length_;
-  const double height_;
+  const int length_;
+  const int height_;
   const int length_of_day_;
+
+  int num_food_;
+  double total_calories_;
+
   // Vector of food objects that spawn everyday
   std::vector<Food> food_;
   // Vector of surviving organisms that exist in the environment
   std::vector<Organism> organisms_;
-
+  // The number of frames that have elapsed since the day began. Gets reset to
+  // zero at the end of each day
+  int time_elapsed_;
   // Helper Methods:
 
   /**
-   * Function to remove organisms that have not survived atthe end of each day
+   * Function to remove organisms that have not survived at the end of each day
    */
   void RemoveDeadOrganisms();
 
@@ -53,5 +118,22 @@ class Environment {
    */
   void AddNewOrganisms();
 
+  /**
+   * Helper function to fill the Food vector of the environment
+   */
+  void AddFood();
+
+  /**
+   * Function to add initial organisms to the environment
+   * @param num_organisms The number of organisms to be added
+   * @param size The size of the organisms
+   * @param speed The speed of the organisms
+   */
+  void AddOrganisms(int num_organisms, double speed, double size);
+
+  /**
+   * Function to reset all organisms at the end of each day of the simulation
+   */
+  void ResetOrganisms();
 };
 } // namespace naturalselection
